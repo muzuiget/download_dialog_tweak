ca65ccf464209fc4fe507038bcb0389e3814e18e
muzuiget <muzuiget@gmail.com>
2017-04-25 15:04:58 +0800

    change global level `let` and `const` to `var`

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,16 +4,16 @@
 
 "use strict";
 
-const log = function() { dump(Array.slice(arguments).join(' ') + '\n'); };
-const trace = function(error) { log(error); log(error.stack); };
-const dirobj = function(obj) { for (let i in obj) { log(i, ':', obj[i]); } };
+var log = function() { dump(Array.slice(arguments).join(' ') + '\n'); };
+var trace = function(error) { log(error); log(error.stack); };
+var dirobj = function(obj) { for (let i in obj) { log(i, ':', obj[i]); } };
 
-const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
-const NS_XUL = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
+var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+var NS_XUL = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
 
 /* library */
 
-const Utils = (function() {
+var Utils = (function() {
 
     const sbService = Cc['@mozilla.org/intl/stringbundle;1']
                          .getService(Ci.nsIStringBundleService);
@@ -64,7 +64,7 @@ const Utils = (function() {
     return exports;
 })();
 
-const DialogManager = (function() {
+var DialogManager = (function() {
 
     const windowWatcher = Cc['@mozilla.org/embedcomp/window-watcher;1']
                              .getService(Ci.nsIWindowWatcher);
@@ -127,7 +127,7 @@ const DialogManager = (function() {
     return exports;
 })();
 
-const Pref = function(branchRoot) {
+var Pref = function(branchRoot) {
 
     const supportsStringClass = Cc['@mozilla.org/supports-string;1'];
     const prefService = Cc['@mozilla.org/preferences-service;1']
@@ -170,12 +170,12 @@ const Pref = function(branchRoot) {
 
 /* main */
 
-let _ = null;
-let loadLocalization = function() {
+var _ = null;
+var loadLocalization = function() {
     _ = Utils.localization('downloaddialogtweak', 'global');
 };
 
-let DownloadDialogTweak = function() {
+var DownloadDialogTweak = function() {
 
     const PREF_BRANCH = 'extensions.downloaddialogtweak.';
 
@@ -401,17 +401,17 @@ let DownloadDialogTweak = function() {
 
 /* bootstrap entry points */
 
-let downloadDialogTweak;
+var downloadDialogTweak;
 
-let install = function(data, reason) {};
-let uninstall = function(data, reason) {};
+var install = function(data, reason) {};
+var uninstall = function(data, reason) {};
 
-let startup = function(data, reason) {
+var startup = function(data, reason) {
     loadLocalization();
     downloadDialogTweak = DownloadDialogTweak();
     downloadDialogTweak.initialize();
 };
 
-let shutdown = function(data, reason) {
+var shutdown = function(data, reason) {
     downloadDialogTweak.destory();
 };
